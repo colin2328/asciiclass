8357cdfe9ba8b455d75e394b7c81afa9fa76f30b
colin <colin2328@gmail.com>
2013-10-18 16:01:32 -0400

    lab6

--- labs/lab6/lab6.py
+++ labs/lab6/lab6.py
@@ -7,7 +7,7 @@ import math
 
 sc = SparkContext("spark://ec2-54-211-27-58.compute-1.amazonaws.com:7077", "Simple App")
 # Replace `lay-k.json` with `*.json` to get a whole lot more data.
-lay = sc.textFile('s3n://AKIAJFDTPC4XX2LVETGA:lJPMR8IqPw2rsVKmsSgniUd+cLhpItI42Z6DCFku@6885public/enron/*.json')
+lay = sc.textFile('s3n://AKIAJFDTPC4XX2LVETGA:lJPMR8IqPw2rsVKmsSgniUd+cLhpItI42Z6DCFku@6885public/enron/lay-k.json')
 
 json_lay = lay.map(lambda x: json.loads(x)).cache()
 

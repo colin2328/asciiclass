11eb33535ae5d65d7a62e203cdb4417f0e3b54e5
colin <colin2328@gmail.com>
2013-10-19 22:15:23 -0400

    sent update

--- labs/lab6/lab6.py
+++ labs/lab6/lab6.py
@@ -42,7 +42,6 @@ print terms_count.take(5)
 
 #loop through each sender_terms_count. calculate tf idf for each
 senders_to_term_count = sender_terms_count.groupBy(lambda sender_term_count: sender_term_count[0][0])
-sender_term_idf = senders_to_term_count.map(compute_tfidf).groupBy(lambda sender_term_count: sender_term_count[0][0])
 
 def compute_tfidf(sender_term_counts):
     for sender_term_count in sender_term_counts:
@@ -50,6 +49,10 @@ def compute_tfidf(sender_term_counts):
         tf = sender_term_count[1]
         tfidf = tf * idfs[term]
         return (sender, term, tfidf)
+        
+sender_term_idf = senders_to_term_count.map(compute_tfidf).groupBy(lambda sender_term_count: sender_term_count[0][0])
+
+
 
 print sender_term_idf.take(5)
 
